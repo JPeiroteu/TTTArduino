502189750ec798aeb8c3dc030e10efc8495f2c58
Jorge Guedes <jorgeguedes16@gmail.com>
2024-03-21 17:47:44 +0000

    added led strip and pressure pads test in esp32

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,27 +1,32 @@
-/*
-cellPins[0][0] 10 | cellPins[0][1] 7 | cellPins[0][2] 4
------------------------------------------------
-cellPins[1][0] 9 | cellPins[1][1] 6 | cellPins[1][2] 3
------------------------------------------------
-cellPins[2][0] 8 | cellPins[2][1] 5 | cellPins[2][2] 2
-*/
+#include <FastLED.h>
+
+#define NUM_LEDS 54
+#define DATA_PIN 13
+
+CRGB leds[NUM_LEDS];
+
 const int cellPins[3][3] = {
-  {2, 3, 4},
-  {5, 6, 7},
-  {8, 9, 10}
+  {23, 19, 4},
+  {22, 18, 2},
+  {21, 5, 15}
 };
 
 void setup() {
+  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS); 
+
   for (int x = 0; x < 3; x++) {
     for (int y = 0; y < 3; y++) {
-      pinMode(cellPins[x][y], INPUT);
+      pinMode(cellPins[x][y], INPUT_PULLUP);
     }
   }
-  
   Serial.begin(9600);
 }
 
 void loop() {
+  fill_solid(leds, NUM_LEDS, CRGB::Red);
+  
+  FastLED.show();
+
   for (int x = 0; x < 3; x++) {
     for (int y = 0; y < 3; y++) {
       if (digitalRead(cellPins[x][y]) == LOW) {
