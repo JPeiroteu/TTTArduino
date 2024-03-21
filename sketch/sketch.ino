9643d8d62f99ee2cb902eb3e7afebc39aea7983b
Jorge Guedes <jorgeguedes16@gmail.com>
2024-03-21 15:47:14 +0000

    added physical board cells test with arduino

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,24 +1,34 @@
-#include <FastLED.h>
-
-#define NUM_LEDS 54 
-#define DATA_PIN 6  
-
-CRGB leds[NUM_LEDS]; 
+/*
+cellPins[0][0] 10 | cellPins[0][1] 7 | cellPins[0][2] 4
+-----------------------------------------------
+cellPins[1][0] 9 | cellPins[1][1] 6 | cellPins[1][2] 3
+-----------------------------------------------
+cellPins[2][0] 8 | cellPins[2][1] 5 | cellPins[2][2] 2
+*/
+const int cellPins[3][3] = {
+  {2, 3, 4},
+  {5, 6, 7},
+  {8, 9, 10}
+};
 
 void setup() {
-  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);  
+  for (int x = 0; x < 3; x++) {
+    for (int y = 0; y < 3; y++) {
+      pinMode(cellPins[x][y], INPUT);
+    }
+  }
+  
+  Serial.begin(9600);
 }
 
 void loop() {
-  fill_solid(leds, NUM_LEDS, CRGB::Red);
-
-  FastLED.show();
-
-  delay(1000);
-
-  fill_solid(leds, NUM_LEDS, CRGB::Black);
-
-  FastLED.show();
-
-  delay(1000);
+  for (int x = 0; x < 3; x++) {
+    for (int y = 0; y < 3; y++) {
+      if (digitalRead(cellPins[x][y]) == LOW) {
+        Serial.print("Test: ");
+        Serial.print(cellPins[x][y]);
+        Serial.print("\n");
+      }
+    }
+  }
 }
