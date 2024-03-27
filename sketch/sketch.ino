da9eee4cefcf32d2686733c43af7ccb1b532435b
Jorge Guedes <jorgeguedes16@gmail.com>
2024-03-27 10:33:23 +0000

    Implemented LED activation to correspond with the physical arrangement of pressure pads and LEDs, triggered by pad activation on the physical Tic-Tac-Toe board

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -5,6 +5,12 @@
 
 CRGB leds[NUM_LEDS];
 
+const int ledMappings[3][3][6] = {
+  {{6, 7, 8, 9, 10, 11}, {24, 25, 26, 27, 28, 29}, {42, 43, 44, 45, 46, 47}},
+  {{3, 4, 5, 12, 13, 14}, {21, 22, 23, 30, 31, 32}, {39, 40, 41, 48, 49, 50}},
+  {{0, 1, 2, 15, 16, 17}, {18, 19, 20, 33, 34, 35}, {36, 37, 38, 51, 52, 53}}
+};
+
 const int cellPins[3][3] = {
   {23, 19, 4},
   {22, 18, 2},
@@ -19,20 +25,27 @@ void setup() {
       pinMode(cellPins[x][y], INPUT_PULLUP);
     }
   }
+
   Serial.begin(9600);
 }
 
 void loop() {
-  fill_solid(leds, NUM_LEDS, CRGB::Red);
-  
-  FastLED.show();
-
   for (int x = 0; x < 3; x++) {
     for (int y = 0; y < 3; y++) {
       if (digitalRead(cellPins[x][y]) == LOW) {
-        Serial.print("Test: ");
-        Serial.print(cellPins[x][y]);
-        Serial.print("\n");
+        Serial.print("Pad de pressão ativado: ");
+        Serial.print("X: ");
+        Serial.print(x);
+        Serial.print(", Y: ");
+        Serial.println(y);
+
+        for (int i = 0; i < 6; i++) {
+          int ledIndex = ledMappings[x][y][i];
+          leds[ledIndex] = CRGB::Red; 
+        }
+        FastLED.show();
+
+        delay(1000);
       }
     }
   }
