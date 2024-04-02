4b2f8ea96b65c3e3cb1c0822d1f05ed1c9e0aa52
Jorge Guedes <jorgeguedes16@gmail.com>
2024-04-02 16:59:37 +0100

    Added functionality for the physical Tic-Tac-Toe board LEDs to display plays from the server

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,4 +1,7 @@
+#include "wifi_credentials.h"
 #include <FastLED.h>
+#include <WiFi.h>
+#include <ArduinoJson.h>
 
 #define NUM_LEDS 54
 #define DATA_PIN 13
@@ -17,7 +20,10 @@ const int cellPins[3][3] = {
   {21, 5, 15}
 };
 
-CRGB currentColor = CRGB::Blue;
+const char* serverAddress = "192.168.1.122";
+const int serverPort = 5000;
+
+WiFiClient client;
 
 void setup() {
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS); 
@@ -29,32 +35,63 @@ void setup() {
   }
 
   Serial.begin(9600);
+
+  connectToWiFi();
 }
 
 void loop() {
-  for (int x = 0; x < 3; x++) {
-    for (int y = 0; y < 3; y++) {
-      if (digitalRead(cellPins[x][y]) == LOW) {
-        Serial.print("Pressure Pad Activited - ");
-        Serial.print("X: ");
-        Serial.print(x);
-        Serial.print(", Y: ");
-        Serial.println(y);
+  String boardState = getBoardState();
+  DynamicJsonDocument doc(1024);
 
-        for (int i = 0; i < 6; i++) {
-          int ledIndex = ledMappings[x][y][i];
-          leds[ledIndex] = currentColor; 
+  JsonArray grid = doc["grid"].as<JsonArray>();
+  for (JsonObject cell : grid) {
+    int x = cell["x"];
+    int y = cell["y"];
+    const char* marker = cell["marker"];
+
+    for (int i = 0; i < 6; i++) {
+      int ledIndex = ledMappings[x][y][i];
+      leds[ledIndex] = (strcmp(marker, "X") == 0) ? CRGB::Blue : (strcmp(marker, "O") == 0) ? CRGB::Green : CRGB::Black;
+    }
+  }
+  
+  FastLED.show();
+  delay(1000); 
+}
+
+void connectToWiFi() {
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Attempting to connect to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+}
+
+String getBoardState() {
+  if (client.connect(serverAddress, serverPort)) {
+    client.print("GET /board HTTP/1.1\r\n");
+    client.print("Host: ");
+    client.print(serverAddress);
+    client.println("\r\nConnection: close\r\n");
+    
+    String response = "";
+    bool isBody = false;
+    
+    while (client.connected() || client.available()) {
+      if (client.available()) {
+        String line = client.readStringUntil('\n');
+        if (line == "\r") {
+          isBody = true;
+        } else if (isBody) {
+          response += line;
         }
-        FastLED.show();
-
-        if (currentColor == CRGB::Blue) {
-          currentColor = CRGB::Yellow; 
-        } else {
-          currentColor = CRGB::Blue; 
-        }
-
-        delay(1000);
       }
     }
+    
+    client.stop();
+    return response;
+  } else {
+    return "";
   }
 }
