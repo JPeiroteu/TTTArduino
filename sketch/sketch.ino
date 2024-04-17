bd161316dfcd25bd1381be92e4a9a98dde592d46
Jorge Guedes <jorgeguedes16@gmail.com>
2024-04-17 15:26:57 +0100

    Added functionality for blinking the winning cells

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -4,7 +4,7 @@
 #include <ArduinoJson.h>
 
 #define NUM_LEDS 54
-#define DATA_PIN 13 // LEDs data pin
+#define DATA_PIN 13 // LED data pin
 
 CRGB leds[NUM_LEDS];
 
@@ -22,31 +22,23 @@ const int cellPins[3][3] = {
   {21, 5, 15}
 };
 
-const char *serverAddress = "192.168.1.122";
-const int serverPort = 5000;
+const char *serverAddress = "94.63.14.247"; //192.168.1.122 LAN, 10.72.73.124 IADE, 94.63.14.247 WLAN
+const int serverPort = 5000;  //5000 or 8000
 
-char currentMarker = 'X'; // X blue LEDs and O green LEDs
-
+String currentPlayer; 
 bool gameStarted = false;
 
 WiFiClient client;
 
 void setup() {
   Serial.begin(9600);
-
   connectToWiFi();
-
-  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS); 
-
-  for (int x = 0; x < 3; x++) {
-    for (int y = 0; y < 3; y++) {
-      pinMode(cellPins[x][y], INPUT_PULLUP);
-    }
-  }
+  initializeBoard();
 }
 
 
 void loop() {
+  
   String boardState = getBoardState();
   
   DynamicJsonDocument doc(1024);
@@ -59,15 +51,24 @@ void loop() {
   }
 
   // Update LEDs based on board state
-  updateLeds(doc);
-
+  updateLeds(doc);   
   // Check any pressed cell
   checkAndMarkCells(doc);
+
+  
+
+  if (!gameStarted) {
+    displayMessage("TIC TAC TOE", doc);
+  }
+
+  checkWinner();
+
+  
 }
 
 void connectToWiFi() {
   Serial.println("Connecting to WiFi...");
-  WiFi.begin(ssid, password);
+  WiFi.begin(ssid, password); //WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
@@ -75,6 +76,16 @@ void connectToWiFi() {
   }
 
   Serial.println("Connected to WiFi");
+}
+
+void initializeBoard() {
+  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS); 
+
+  for (int x = 0; x < 3; x++) {
+    for (int y = 0; y < 3; y++) {
+      pinMode(cellPins[x][y], INPUT_PULLUP);
+    }
+  }
 }
 
 String getBoardState() {
@@ -108,10 +119,6 @@ String getBoardState() {
 
 void updateLeds(JsonDocument &doc) {
   JsonArray grid = doc["grid"].as<JsonArray>();
-  
-  if (!gameStarted) {
-    displayMessage("TIC TAC TOE");
-  }
 
   for (JsonObject cell : grid) {
     int x = cell["x"];
@@ -128,34 +135,72 @@ void updateLeds(JsonDocument &doc) {
   delay(100);
 }
 
-void checkAndMarkCells(JsonDocument &doc) {
-  JsonArray grid = doc["grid"].as<JsonArray>();
-
-  if (!gameStarted) {
-    for (JsonObject cell : grid) {
-      const char* marker = cell["marker"];
-      if (strcmp(marker, " ") != 0) {
-        gameStarted = true;
-        break;
-      }
-    }
-  }
-
-  if (gameStarted) {
-    for (int x = 0; x < 3; x++) {
-      for (int y = 0; y < 3; y++) {
-        if (digitalRead(cellPins[x][y]) == LOW) {
-          markCell(x, y, currentMarker);
-          currentMarker = (currentMarker == 'X') ? 'O' : 'X';
-          delay(500); 
+String getCurrentPlayer() {
+  if (client.connect(serverAddress, serverPort)) {
+    client.print("GET /player/current HTTP/1.1\r\n");
+    client.print("Host: ");
+    client.print(serverAddress);
+    client.println("\r\nConnection: close\r\n");
+    
+    String response = "";
+    bool isBody = false;
+    
+    while (client.connected() || client.available()) {
+      if (client.available()) {
+        String line = client.readStringUntil('\n');
+        if (line == "\r") {
+          isBody = true;
+        } else if (isBody) {
+          response += line; // JSON body
         }
       }
     }
+    client.stop();
+    Serial.println(response);
+
+    DynamicJsonDocument doc(1024);
+    deserializeJson(doc, response);
+    currentPlayer = doc["currentPlayer"].as<String>();  
+
+    Serial.print("O jogador atual é: ");
+    Serial.println(currentPlayer);
+
+    return currentPlayer; // Retorna o jogador atual como uma String
+  } else {
+    Serial.println("Failed to connect to server");
+    return "";
   }
 }
 
-void displayMessage(String message) {
-  int letterIndex = 0;
+bool checkAndMarkCells(JsonDocument &doc) {
+  JsonArray grid = doc["grid"].as<JsonArray>();
+
+  for (JsonObject cell : grid) {
+    const char* marker = cell["marker"];
+    if (strcmp(marker, " ") != 0) {
+      gameStarted = true;
+      break;
+    } else {
+        gameStarted = false;
+    }
+
+  }
+
+  for (int x = 0; x < 3; x++) {
+    for (int y = 0; y < 3; y++) {
+      if (digitalRead(cellPins[x][y]) == LOW) {
+        currentPlayer = getCurrentPlayer();
+        markCell(x, y, currentPlayer);
+        gameStarted = true;
+        delay(500); 
+      }
+    }
+  }
+  return gameStarted;
+}
+
+
+void displayMessage(String message, JsonDocument &doc) {
   for (int i = 0; i < message.length(); i++) {
     switch (message[i]) {
       case 'T':
@@ -179,6 +224,10 @@ void displayMessage(String message) {
       case ' ':
         break;
     }
+    if (checkAndMarkCells(doc)) {
+      return;
+    }
+
     delay(1000);
     clearLeds(); 
   }
@@ -270,7 +319,7 @@ void clearLeds() {
   FastLED.show(); 
 }
 
-void markCell(int x, int y, char marker) {
+void markCell(int x, int y, String marker) {
   String postData = "x=" + String(x) + "&y=" + String(y) + "&mark=" + String(marker);
 
   sendPostRequest("/cell/mark", postData);
@@ -296,4 +345,69 @@ void sendPostRequest(String path, String data) {
   } else {
     Serial.println("Failed to connect to server");
   }
+}
+
+void checkWinner() {
+  if (client.connect(serverAddress, serverPort)) {
+    client.println("GET /check_winner HTTP/1.1");
+    client.print("Host: ");
+    client.println(serverAddress);
+    client.println("Connection: close");
+    client.println();
+
+    
+    String response = "";
+    bool isBody = false;
+    
+    while (client.connected() || client.available()) {
+      if (client.available()) {
+        String line = client.readStringUntil('\n');
+        if (line == "\r") {
+          isBody = true;
+        } else if (isBody) {
+          response += line;
+        }
+      }
+    }
+    client.stop();
+    
+    // Parse response to check for winner
+    if (response.length() > 0) {
+      // Parse JSON response
+      DynamicJsonDocument doc(1024);
+      DeserializationError error = deserializeJson(doc, response);
+      
+      if (!error) {
+        JsonObject winnerData = doc["win_cell"];
+        if (!winnerData.isNull()) {
+          // Extract winner's cells coordinates
+          int x1 = winnerData["x"];
+          int y1 = winnerData["y"];
+          JsonObject winnerData2 = doc["win_cell2"];
+          int x2 = winnerData2["x"];
+          int y2 = winnerData2["y"];
+          JsonObject winnerData3 = doc["win_cell3"];
+          int x3 = winnerData3["x"];
+          int y3 = winnerData3["y"];
+          
+          // Make winning cells blink
+          blinkWinningCells(x1, y1, x2, y2, x3, y3);
+          Serial.println(response);
+        }
+      }
+    }
+  }
+}
+void blinkWinningCells(int x1, int y1, int x2, int y2, int x3, int y3) {
+  for (int i = 0; i < 6; i++) {
+    int ledIndex1 = ledMappings[x1][y1][i];
+    int ledIndex2 = ledMappings[x2][y2][i];
+    int ledIndex3 = ledMappings[x3][y3][i];
+    
+    leds[ledIndex1] = CRGB::Black; // Turn on LED 1
+    leds[ledIndex2] = CRGB::Black; // Turn on LED 2
+    leds[ledIndex3] = CRGB::Black; // Turn on LED 3
+  }
+  FastLED.show();
+  delay(300); // Blink duration
 }
